2314fb4855fd5444be4d9197172ac874a57a7063
Yadwinder <ryker1990@gmail.com>
2016-04-15 21:09:36 +0530

    first commit

---
+++ Arduino/PCA9536_R11.ino
@@ -0,0 +1,68 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// PCA9536_R11
+// This code is designed to work with the PCA9536_I2CR_R11 I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/products
+
+#include<Wire.h>
+
+// PCA9536_R11 I2C address is 0x41(65)
+#define Addr 0x41
+
+void setup()
+{
+  // Initialise I2C communication
+  Wire.begin();
+  // Initialise Serial Communication, set baud rate = 9600
+  Serial.begin(9600);
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select configuration register
+  Wire.write(0x03);
+  // Set all pins as OUTPUT
+  Wire.write(0x00);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select output port register
+  Wire.write(0x01);
+  // Set Pin-1 HIGH
+  Wire.write(0x01);
+  // Stop I2C transmission
+  Wire.endTransmission();
+  delay(300);
+}
+
+void loop()
+{
+  unsigned int output;
+
+  // Start I2C transmission
+  Wire.beginTransmission(Addr);
+  // Select data register
+  Wire.write(0x01);
+  // Stop I2C transmission
+  Wire.endTransmission();
+
+  // Request 1 byte of data
+  Wire.requestFrom(Addr, 1);
+
+  // Read 1 byte of data from address 0x01(01)
+  if (Wire.available() == 1)
+  {
+    output = Wire.read();
+  }
+
+  if ((output & 0x01)
+  {
+    Serial.println("Pin-1 : HIGH");
+  }
+  else
+  {
+    Serial.println("Pin-1 : LOW");
+  }
+  delay(500);
+}
